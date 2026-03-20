721fbf1dcc64d2e033fb8cdda40fcce74218db94
Dilshan <dilshananupriya@icloud.com>
2026-03-21 04:11:18 +0530

    register ureka server

--- src/main/java/vehicle_service/CarRentalApplication.java
+++ src/main/java/vehicle_service/CarRentalApplication.java
@@ -2,8 +2,10 @@ package vehicle_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarRentalApplication {
 
 	public static void main(String[] args) {
